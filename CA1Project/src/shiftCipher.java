a4da17984a7746fbecfd656a86d9204a806b408f
DavidCampion <D00238516@student.dkit.ie>
2021-12-15 20:25:34 +0000

    Created Menu...

--- CA1Project/src/shiftCipher.java
+++ CA1Project/src/shiftCipher.java
@@ -7,21 +7,15 @@ public class shiftCipher {
 
     public static void main(String[] args) throws IOException {
 
-        File file = new File("CA1Project/cipher.txt");
-        Scanner fin = new Scanner(file);
         Scanner kb = new Scanner(System.in);
-
-        String cipherOne = fin.nextLine().toUpperCase();
-        String cipherTwo = fin.nextLine().toUpperCase();
-
 
         int choice = 0;
         boolean isNumber;
         while (choice != 3) {
-            System.out.println("====== Cipher Decrypter 9000 ======");
+            System.out.println("====== Cipher Decrypter & Encrypter 9001 ======");
             System.out.println();
-            System.out.println("1. Decrypt The first Cipher from file");
-            System.out.println("2. Decrypt The Second Cipher from file (Using)");
+            System.out.println("1. Encrypt a File");
+            System.out.println("2. Decrypt a File");
             System.out.println("3. Exit");
             System.out.println();
             System.out.println("====================================");
@@ -34,12 +28,12 @@ public class shiftCipher {
                     isNumber = true;
                     switch (choice) {
                         case 1:
-                            System.out.println("Cracking First Cipher....");
-                            cipherOnePrint(cipherOne);
+                            System.out.println("Encrypt File Selected... ");
+
                             break;
                         case 2:
-                            System.out.println("Cracking Second Cipher....");
-                            cipherTwoPrint(cipherTwo);
+                            System.out.println("Decrypt File Selected... ");
+
                             break;
                         case 3:
                             System.out.println("Goodbye");
@@ -53,33 +47,6 @@ public class shiftCipher {
             } while (!(isNumber));
             kb.nextLine();
         }
-    }
-
-    public static void cipherOnePrint(String s) {
-        System.out.println();
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("Original Cipher = " + s);
-        System.out.println("Cipher Shift Key = 3");
-        System.out.println("Uncoded Cipher = " + cipher(s, 3));
-        System.out.println("-------------------------------------------------------------------------");
-    }
-
-    public static void cipherTwoPrint(String s) {
-        System.out.println();
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("Original Cipher = " + s);
-        int cipherCount = 0;
-        for(int i = 0; i <= 26; i++){
-            String cipherCrack = cipher(s, i);
-            if (!cipherCrack.contains("DONE")){
-                cipherCount++;
-            } else {
-                System.out.println("Shift Key is " + cipherCount);
-                System.out.println("Uncoded Cipher = " + cipherCrack);
-                i = 26;
-            }
-        }
-        System.out.println("-------------------------------------------------------------------------");
     }
 
     static String cipher(String msg, int shift){
